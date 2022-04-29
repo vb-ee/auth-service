ae441780226bce9c6912556be954afe72b711328
Vekil Bekmuradov <bekmuradovimprove@gmail.com>
2022-04-29 07:46:02 +0500

    Add coverage for test suites

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -18,37 +18,32 @@ router.post(
                 .send({ success: false, message: 'User already exists' })
         }
 
-        try {
-            const result = await sequelize.transaction(async () => {
-                const newUser = await User.create({ email, password })
-                const jwtPayload = { email }
-                const accessToken = JwtUtils.generateAccessToken(jwtPayload)
-                const refreshToken = JwtUtils.generateRefreshToken(jwtPayload)
-                await newUser.createRefreshToken({ token: refreshToken })
+        const result = await sequelize.transaction(async () => {
+            const newUser = await User.create({ email, password })
+            const jwtPayload = { email }
+            const accessToken = JwtUtils.generateAccessToken(jwtPayload)
+            const refreshToken = JwtUtils.generateRefreshToken(jwtPayload)
+            await newUser.createRefreshToken({ token: refreshToken })
 
-                if (roles && Array.isArray(roles)) {
-                    const rolesToSave = []
-                    for (const role of roles) {
-                        const newRole = await Role.create({ role })
-                        rolesToSave.push(newRole)
-                    }
-                    await newUser.addRoles(rolesToSave)
+            if (roles && Array.isArray(roles)) {
+                const rolesToSave = []
+                for (const role of roles) {
+                    const newRole = await Role.create({ role })
+                    rolesToSave.push(newRole)
                 }
+                await newUser.addRoles(rolesToSave)
+            }
 
-                return { accessToken, refreshToken }
-            })
+            return { accessToken, refreshToken }
+        })
 
-            const { accessToken, refreshToken } = result
+        const { accessToken, refreshToken } = result
 
-            return res.send({
-                success: true,
-                message: 'User successfully created',
-                data: { accessToken, refreshToken }
-            })
-        } catch (error) {
-            console.error('Error signing up the user: \n', error.stack)
-            res.status(500).send({ success: false, message: error.message })
-        }
+        return res.send({
+            success: true,
+            message: 'User successfully created',
+            data: { accessToken, refreshToken },
+        })
     })
 )
 
